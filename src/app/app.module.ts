ccb4bfdcc9e826c5d566b40f7cb2d52e611ac31f
Samuel Tijani <omalitijani@gmail.com>
2020-09-03 16:58:31 +0100

    Generate navigation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PropertyCardComponent } from './property/property-card/property-card.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PropertyCardComponent
-  ],
+    PropertyCardComponent,
+    NavBarComponent
+   ],
   imports: [
     BrowserModule,
     AppRoutingModule
