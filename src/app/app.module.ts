3f7c88c5a2ba9047655c9781205ee5d451f0fa40
Samuel Tijani <omalitijani@gmail.com>
2020-09-03 16:56:44 +0100

    Generate property card

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PropertyCardComponent } from './property/property-card/property-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PropertyCardComponent
   ],
   imports: [
     BrowserModule,
