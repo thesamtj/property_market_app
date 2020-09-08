1b0af8e486b22d4e4aa50bdb2f107745566aa944
Samuel Tijani <omalitijani@gmail.com>
2020-09-08 19:18:49 +0100

    Move to List component

--- src/app/property/property-list/property-list.component.ts
+++ src/app/property/property-list/property-list.component.ts
@@ -6,6 +6,44 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./property-list.component.scss']
 })
 export class PropertyListComponent implements OnInit {
+  property:Array<any> = [
+    {
+    "id": 1,
+    "name": "SamTJ",
+    "type": "house",
+    "price": 5000
+  },
+  {
+    "id": 2,
+    "name": "SamTJ",
+    "type": "house",
+    "price": 5000
+  },
+  {
+    "id": 3,
+    "name": "SamTJ",
+    "type": "house",
+    "price": 5000
+  },
+  {
+    "id": 4,
+    "name": "SamTJ",
+    "type": "house",
+    "price": 5000
+  },
+  {
+    "id": 5,
+    "name": "SamTJ",
+    "type": "house",
+    "price": 5000
+  },
+  {
+    "id": 6,
+    "name": "SamTJ",
+    "type": "house",
+    "price": 5000
+  }
+]
 
   constructor() { }
 
