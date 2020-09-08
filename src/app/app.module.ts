0a6a6150f70480618d4567520183a076a1196053
Samuel Tijani <omalitijani@gmail.com>
2020-09-08 13:13:00 +0100

    Add newly created component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PropertyCardComponent } from './property/property-card/property-card.component';
+import { PropertyListComponent } from './property/property-list/property-list.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    NavBarComponent,
     PropertyCardComponent,
-    NavBarComponent
+    PropertyListComponent
    ],
   imports: [
     BrowserModule,
