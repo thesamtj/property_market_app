e253c79197521750f67a6ef45d8688bb2c6755a2
Samuel Tijani <omalitijani@gmail.com>
2020-09-09 12:02:07 +0100

    Create json file

--- src/app/property/property-list/property-list.component.ts
+++ src/app/property/property-list/property-list.component.ts
@@ -7,44 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PropertyListComponent implements OnInit {
 
-  properties:Array<any> = [
-    {
-    "id": 1,
-    "name": "SamTJ",
-    "type": "house1",
-    "price": 5000
-  },
-  {
-    "id": 2,
-    "name": "SamTJ",
-    "type": "house2",
-    "price": 6000
-  },
-  {
-    "id": 3,
-    "name": "SamTJ",
-    "type": "house3",
-    "price": 7000
-  },
-  {
-    "id": 4,
-    "name": "SamTJ",
-    "type": "house4",
-    "price": 8000
-  },
-  {
-    "id": 5,
-    "name": "SamTJ",
-    "type": "house5",
-    "price": 9000
-  },
-  {
-    "id": 6,
-    "name": "SamTJ",
-    "type": "house6",
-    "price": 3000
-  }
-]
+  properties:Array<any>;
 
   constructor() { }
 
