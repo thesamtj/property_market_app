c0e471caf7fd90dd77cebbb81625149035cad3f9
Samuel Tijani <omalitijani@gmail.com>
2020-09-29 10:31:49 +0100

    Define routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: PropertyListComponent},
+  {path: 'rent-property', component: PropertyListComponent},
+  {path: 'add-property', component: AddPropertyComponent},
+  {path: 'property-detail/:id',
+          component: PropertyDetailComponent,
+          resolve: {prp: PropertyDetailResolverService}},
+  {path: 'user/login', component: UserLoginComponent},
+  {path: 'user/register', component: UserRegisterComponent},
+  {path: '**', component: PropertyListComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
