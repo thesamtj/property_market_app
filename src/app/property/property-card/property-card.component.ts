e53928506a168f0a50e930e3d94c3c04102ba98d
Samuel Tijani <omalitijani@gmail.com>
2020-09-29 14:47:16 +0100

    Implement card component

--- src/app/property/property-card/property-card.component.ts
+++ src/app/property/property-card/property-card.component.ts
@@ -1,18 +1,11 @@
-import { Component, OnInit, Input } from '@angular/core';
-import { IProperty } from '../../model/property';
-
+import { Component, Input } from '@angular/core';
+import { IPropertyBase } from 'src/app/model/ipropertybase';
 @Component({
   selector: 'app-property-card',
   templateUrl: './property-card.component.html',
-  styleUrls: ['./property-card.component.scss']
+  styleUrls: ['./property-card.component.scss'],
 })
-export class PropertyCardComponent implements OnInit {
-  @Input()
-  property:IProperty
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class PropertyCardComponent {
+  @Input() property: IPropertyBase;
+  @Input() hideIcons: boolean;
 }
