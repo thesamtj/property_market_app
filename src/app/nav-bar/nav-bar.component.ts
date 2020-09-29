5613bb7be018e02df79c7172abf34baa1002ba4e
Samuel Tijani <omalitijani@gmail.com>
2020-09-29 10:38:52 +0100

    Implement nav bar

--- src/app/nav-bar/nav-bar.component.ts
+++ src/app/nav-bar/nav-bar.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { AlertifyService } from '../services/alertify.service';
 
 @Component({
   selector: 'app-nav-bar',
   templateUrl: './nav-bar.component.html',
-  styleUrls: ['./nav-bar.component.scss']
+  styleUrls: ['./nav-bar.component.scss'],
 })
 export class NavBarComponent implements OnInit {
+  loggedinUser: string;
+  constructor(private alertify: AlertifyService) {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  loggedin() {
+    this.loggedinUser = localStorage.getItem('token');
+    return this.loggedinUser;
   }
 
+  onLogout() {
+    localStorage.removeItem('token');
+    this.alertify.success('You are logged out !');
+  }
 }
